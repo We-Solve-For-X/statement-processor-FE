0fd8718aab8df56b9e2ff589a64badcb33740f76
Mardu Swanepoel <mardu.swanepoel@spandigital.com>
2019-12-27 08:50:48 +0200

    cleanup

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ea648a71b05eed8da46";
+/******/ 	var hotCurrentHash = "3648c93029423d06be99";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
