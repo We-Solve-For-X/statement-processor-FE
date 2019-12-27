e912facf9586247a5d082a9ec7b3d806333ea438
Mardu Swanepoel <mardu.swanepoel@spandigital.com>
2019-12-27 09:31:52 +0200

    fix name

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,11 +1,5 @@
 import Header from './Header';
 
-const layoutStyle = {
-  // margin: 20,
-  // padding: 20,
-  // border: '1px solid #DDD'
-};
-
 const PageLayout = props => (
   <div style={{width: "100vw", height: "100vh", backgroundColor: "#DEDEDE"}}>
     <Header />
